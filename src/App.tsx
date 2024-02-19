a30c70683b68c03a22d7c6f2339952ea1de594a4
Dziben <56271191+Dziben@users.noreply.github.com>
2024-02-19 03:53:55 +0100

    set up margins, background and padding

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from "react";
-import "./App.css";
 
-function App() {
+import Header from "./Header";
+import Tab from "./Tab";
+
+import "./App.css";
+import Colors from "./Colors";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="/logo.svg" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Tab>
+        <Colors />
+      </Tab>
     </div>
   );
-}
+};
 
 export default App;
