9a7569807887ceb20bff9aca246c1fbd6ceeb27b
Dziben <56271191+Dziben@users.noreply.github.com>
2024-02-19 03:57:44 +0100

    colors tab wip

---
+++ src/Colors.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Colors.css";
+
+const Colors = () => {
+  return (
+    <div className="colors-wrapper">
+      <textarea></textarea>
+      <button></button>
+    </div>
+  );
+};
+
+export default Colors;
